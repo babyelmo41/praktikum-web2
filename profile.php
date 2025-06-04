85817ef2a6d77ad9ee7842bedf089098dcc6d90c
Aulia Izza Fahlevi <izza.fahlevi@gmail.com>
2025-06-04 20:25:09 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,53 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/izza.jpg" alt="Aulia Izza Fahlevi" width="160" class="rounded-circle img-thumbnail mt-4">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NPM :</strong></td>
+                        <td align="left" width="50%">11 2222 333 / 2310020002</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Sistem Informasi</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Izza Fahlevi</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Tangerang, 18 April 2000</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0895 2022 1853</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">izza.fahlevi@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/izzanaxsatoela" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/izza_fahlevi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/izza_fahlevii" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-twitter-circled.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/dedyrosyadi" target="_blank">
+                                <img src="img/medsos/icons8-linkedin-circled.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
